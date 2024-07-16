a482239816789d8ba87e4d92385b144d93aa84ce
20-414 <amireddydivya7@gmail.com>
2024-07-16 10:46:26 +0530

    Add files via upload

---
+++ Add.c
@@ -0,0 +1,24 @@
+#include"myheader.h"
+
+STUDENT * AddFirst(STUDENT *head)
+{
+	STUDENT *newnode=NULL;
+	newnode=malloc(sizeof(STUDENT));
+	if(newnode==NULL)
+	{
+		printf("Node not created\n");
+	}
+	else
+	{
+		printf("enter the roll\n");
+		scanf("%d",&newnode->roll);
+		printf("enter the name\n");
+		scanf("%s",newnode->name);
+		newnode->link=NULL;
+
+		newnode->link=head;
+		head=newnode;
+	}
+	return head;
+
+}
